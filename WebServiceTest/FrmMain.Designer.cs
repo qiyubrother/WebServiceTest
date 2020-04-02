fbb1c2c1d900eb814c16c46def4f4629b7b70dd3
13240137763@163.com <qiyubrother>
2020-04-02 17:03:42 +0800

    添加项目文件。

---
+++ WebServiceTest/FrmMain.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WebServiceTest
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtMethod = new System.Windows.Forms.TextBox();
+            this.btnCall = new System.Windows.Forms.Button();
+            this.txtParameter1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtParameter2 = new System.Windows.Forms.TextBox();
+            this.txtParameter3 = new System.Windows.Forms.TextBox();
+            this.txtParameter4 = new System.Windows.Forms.TextBox();
+            this.txtParameter8 = new System.Windows.Forms.TextBox();
+            this.txtParameter7 = new System.Windows.Forms.TextBox();
+            this.txtParameter6 = new System.Windows.Forms.TextBox();
+            this.txtParameter5 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Method";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(12, 25);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(506, 21);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // txtMethod
+            // 
+            this.txtMethod.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMethod.Location = new System.Drawing.Point(12, 67);
+            this.txtMethod.Name = "txtMethod";
+            this.txtMethod.Size = new System.Drawing.Size(506, 21);
+            this.txtMethod.TabIndex = 3;
+            // 
+            // btnCall
+            // 
+            this.btnCall.Location = new System.Drawing.Point(12, 327);
+            this.btnCall.Name = "btnCall";
+            this.btnCall.Size = new System.Drawing.Size(157, 23);
+            this.btnCall.TabIndex = 13;
+            this.btnCall.Text = "Call Web Method";
+            this.btnCall.UseVisualStyleBackColor = true;
+            this.btnCall.Click += new System.EventHandler(this.btnCall_Click);
+            // 
+            // txtParameter1
+            // 
+            this.txtParameter1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParameter1.Location = new System.Drawing.Point(12, 111);
+            this.txtParameter1.Name = "txtParameter1";
+            this.txtParameter1.Size = new System.Drawing.Size(506, 21);
+            this.txtParameter1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Parameters";
+            // 
+            // txtParameter2
+            // 
+            this.txtParameter2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParameter2.Location = new System.Drawing.Point(12, 138);
+            this.txtParameter2.Name = "txtParameter2";
+            this.txtParameter2.ReadOnly = true;
+            this.txtParameter2.Size = new System.Drawing.Size(506, 21);
+            this.txtParameter2.TabIndex = 6;
+            // 
+            // txtParameter3
+            // 
+            this.txtParameter3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParameter3.Location = new System.Drawing.Point(12, 165);
+            this.txtParameter3.Name = "txtParameter3";
+            this.txtParameter3.ReadOnly = true;
+            this.txtParameter3.Size = new System.Drawing.Size(506, 21);
+            this.txtParameter3.TabIndex = 7;
+            // 
+            // txtParameter4
+            // 
+            this.txtParameter4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParameter4.Location = new System.Drawing.Point(12, 192);
+            this.txtParameter4.Name = "txtParameter4";
+            this.txtParameter4.ReadOnly = true;
+            this.txtParameter4.Size = new System.Drawing.Size(506, 21);
+            this.txtParameter4.TabIndex = 8;
+            // 
+            // txtParameter8
+            // 
+            this.txtParameter8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParameter8.Location = new System.Drawing.Point(12, 300);
+            this.txtParameter8.Name = "txtParameter8";
+            this.txtParameter8.ReadOnly = true;
+            this.txtParameter8.Size = new System.Drawing.Size(506, 21);
+            this.txtParameter8.TabIndex = 12;
+            // 
+            // txtParameter7
+            // 
+            this.txtParameter7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParameter7.Location = new System.Drawing.Point(12, 273);
+            this.txtParameter7.Name = "txtParameter7";
+            this.txtParameter7.ReadOnly = true;
+            this.txtParameter7.Size = new System.Drawing.Size(506, 21);
+            this.txtParameter7.TabIndex = 11;
+            // 
+            // txtParameter6
+            // 
+            this.txtParameter6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParameter6.Location = new System.Drawing.Point(12, 246);
+            this.txtParameter6.Name = "txtParameter6";
+            this.txtParameter6.ReadOnly = true;
+            this.txtParameter6.Size = new System.Drawing.Size(506, 21);
+            this.txtParameter6.TabIndex = 10;
+            // 
+            // txtParameter5
+            // 
+            this.txtParameter5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParameter5.Location = new System.Drawing.Point(12, 219);
+            this.txtParameter5.Name = "txtParameter5";
+            this.txtParameter5.ReadOnly = true;
+            this.txtParameter5.Size = new System.Drawing.Size(506, 21);
+            this.txtParameter5.TabIndex = 9;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(12, 356);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(506, 201);
+            this.txtResult.TabIndex = 14;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 570);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtParameter8);
+            this.Controls.Add(this.txtParameter7);
+            this.Controls.Add(this.txtParameter6);
+            this.Controls.Add(this.txtParameter5);
+            this.Controls.Add(this.txtParameter4);
+            this.Controls.Add(this.txtParameter3);
+            this.Controls.Add(this.txtParameter2);
+            this.Controls.Add(this.txtParameter1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCall);
+            this.Controls.Add(this.txtMethod);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WebService Test";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtMethod;
+        private System.Windows.Forms.Button btnCall;
+        private System.Windows.Forms.TextBox txtParameter1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtParameter2;
+        private System.Windows.Forms.TextBox txtParameter3;
+        private System.Windows.Forms.TextBox txtParameter4;
+        private System.Windows.Forms.TextBox txtParameter8;
+        private System.Windows.Forms.TextBox txtParameter7;
+        private System.Windows.Forms.TextBox txtParameter6;
+        private System.Windows.Forms.TextBox txtParameter5;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ WebServiceTest/FrmMain.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using qiyubrother;
+
+namespace WebServiceTest
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            txtParameter1.TextChanged += (o, ex) => { txtParameter2.ReadOnly = (o as TextBox).Text == string.Empty; };
+            txtParameter2.TextChanged += (o, ex) => { txtParameter3.ReadOnly = (o as TextBox).Text == string.Empty; };
+            txtParameter3.TextChanged += (o, ex) => { txtParameter4.ReadOnly = (o as TextBox).Text == string.Empty; };
+            txtParameter4.TextChanged += (o, ex) => { txtParameter5.ReadOnly = (o as TextBox).Text == string.Empty; };
+            txtParameter5.TextChanged += (o, ex) => { txtParameter6.ReadOnly = (o as TextBox).Text == string.Empty; };
+            txtParameter6.TextChanged += (o, ex) => { txtParameter7.ReadOnly = (o as TextBox).Text == string.Empty; };
+            txtParameter7.TextChanged += (o, ex) => { txtParameter8.ReadOnly = (o as TextBox).Text == string.Empty; };
+        }
+
+        private void btnCall_Click(object sender, EventArgs e)
+        {
+            var lstParameters = new List<string>();
+            if (txtParameter1.Text != string.Empty)
+            {
+                lstParameters.Add(txtParameter1.Text);
+            }
+            if (!txtParameter2.ReadOnly && txtParameter2.Text != string.Empty)
+            {
+                lstParameters.Add(txtParameter2.Text);
+            }
+            if (!txtParameter3.ReadOnly && txtParameter3.Text != string.Empty)
+            {
+                lstParameters.Add(txtParameter3.Text);
+            }
+            if (!txtParameter4.ReadOnly && txtParameter4.Text != string.Empty)
+            {
+                lstParameters.Add(txtParameter4.Text);
+            }
+            if (!txtParameter5.ReadOnly && txtParameter5.Text != string.Empty)
+            {
+                lstParameters.Add(txtParameter5.Text);
+            }
+            if (!txtParameter6.ReadOnly && txtParameter6.Text != string.Empty)
+            {
+                lstParameters.Add(txtParameter6.Text);
+            }
+            if (!txtParameter7.ReadOnly && txtParameter7.Text != string.Empty)
+            {
+                lstParameters.Add(txtParameter7.Text);
+            }
+            if (!txtParameter8.ReadOnly && txtParameter8.Text != string.Empty)
+            {
+                lstParameters.Add(txtParameter8.Text);
+            }
+            try
+            {
+                var rst = WebServiceHelper.CallMethod(txtUrl.Text, txtMethod.Text, lstParameters.ToArray());
+                txtResult.Text = rst;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                txtResult.Clear();
+            }
+        }
+    }
+}
